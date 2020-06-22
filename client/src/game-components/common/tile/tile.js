3375418a355bd184ad9457bb7e309ad261af7f96
Josh Vaughan <josh.vaughan@outlook.com>
2020-06-22 11:37:12 +0100

    Fix tile image structure

--- client/src/game-components/common/tile/tile.js
+++ client/src/game-components/common/tile/tile.js
@@ -10,16 +10,18 @@ export default function Card(props) {
       style={{ transform: `translate(${props.posX || 0}%, ${props.posY || 0}%)`, zIndex: props.zIndex }}>
       <div className='tile-x-plane' style={{ transform: `rotateX(${props.rotateX || 0}deg)` }}>
         <div className='tile-y-plane' style={{ transform: `rotateY(${props.rotateY || 0}deg)` }}>
-          {
-            props.frontImg ?
-              <img className={`tile-front tile-image ${props.frontImgClass || ''}`} src={props.frontImg} alt="card front" style={imgStyle} /> :
-              <div className={`tile-front tile-image ${props.frontImgClass}`} style={imgStyle} />
-          }
-          {
-            props.backImg ?
-              <img className={`tile-back tile-image ${props.backImgClass || ''}`} src={props.backImg} alt="card back" style={imgStyle} /> :
-              <div className={`tile-back tile-image ${props.backImgClass || ''}`} style={imgStyle} />
-          }
+          <div className={`tile-front tile-image ${props.frontImgClass}`} style={imgStyle}>
+            {
+              props.frontImg &&
+              <img src={props.frontImg} alt="card front" />
+            }
+          </div>
+          <div className={`tile-back tile-image ${props.backImgClass || ''}`} style={imgStyle}>
+            {
+              props.backImg &&
+              <img src={props.backImg} alt="card back" />
+            }
+          </div>
         </div>
       </div>
     </div>
