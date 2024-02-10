347701ac1134a2042584c0bfeb47c39271e3d94b
Josh Vaughan <josh.vaughan@outlook.com>
2024-02-10 19:11:57 +0000

    Fix

--- server/cron.js
+++ server/cron.js
@@ -5,9 +5,9 @@ const url = process.env.PUBLIC_URL;
 const job = new cron.CronJob('*/10 * * * *', function () {
     console.log('Hitting server');
 
-    https.get(url), (res) => {
+    https.get(url, (res) => {
         console.log(res.statusCode)
-    };
+    });
 })
 
 module.exports = job;
